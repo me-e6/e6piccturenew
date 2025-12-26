97d1abba0e90747f92e0a0ad349c175a05d7277e
sukhinder reddy <megithube6@gmail.com>
2025-12-26 21:58:12 +0530

    Before Claude Full revamp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:e6piccturenew/picctureapp.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:e6piccturenew/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
